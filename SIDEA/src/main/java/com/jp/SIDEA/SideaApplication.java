b230796cac04de72d9fdcafe2423f6051c322036
Andre-Luiz01 <105212889+Andre-Luiz01@users.noreply.github.com>
2023-11-25 17:24:15 -0300

    Add files via upload

---
+++ SIDEA/src/main/java/com/jp/SIDEA/SideaApplication.java
@@ -0,0 +1,13 @@
+package com.jp.SIDEA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideaApplication.class, args);
+	}
+
+}
---
+++ SIDEA/src/test/java/com/jp/SIDEA/SideaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.SIDEA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
