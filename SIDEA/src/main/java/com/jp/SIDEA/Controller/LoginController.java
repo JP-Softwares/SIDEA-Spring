55e7dbe74a78ddec01920554f62fe2cec33fe7b7
Andre-Luiz01 <andreff.net@gmail.com>
2023-11-28 21:24:32 -0300

    commit de hoje

--- SIDEA/src/main/java/com/jp/SIDEA/Controller/LoginController.java
+++ SIDEA/src/main/java/com/jp/SIDEA/Controller/LoginController.java
@@ -2,8 +2,14 @@ package com.jp.SIDEA.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.jp.SIDEA.Models.Records.loginJson;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/Login")
@@ -11,14 +17,20 @@ public class LoginController {
 
 
     @GetMapping
-    public ModelAndView denunciaView(){
-        ModelAndView modelinho = new ModelAndView("login");
+    public ModelAndView LoginView(){
+        ModelAndView modelinho = new ModelAndView("login/login");
         return modelinho;
     }
 
-    @GetMapping("/teste")
-    public ModelAndView denunciaViewTeste(){
-        ModelAndView modelinho = new ModelAndView("oi");
+    @GetMapping("/create")
+    public ModelAndView createView(){
+        ModelAndView modelinho = new ModelAndView("login/create");
+        return modelinho;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView create(@ModelAttribute loginJson json, HttpServletRequest request){
+        ModelAndView modelinho = new ModelAndView("login/create");
         return modelinho;
     }
 }
