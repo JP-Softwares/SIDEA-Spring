4bad017ce0cb8799e4627e9a93a5eb5494136934
Andre-Luiz01 <andreff.net@gmail.com>
2023-12-02 16:42:30 -0300

    commit arrumado

--- src/main/java/com/jp/SIDEA/Controller/DenunciaController.java
+++ src/main/java/com/jp/SIDEA/Controller/DenunciaController.java
@@ -1,18 +1,23 @@
 package com.jp.SIDEA.Controller;
 
-import com.jp.SIDEA.Models.Records.loginJson;
+import com.jp.SIDEA.Models.Records.denunciajson;
+import com.jp.SIDEA.Services.DenunciaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-//import com.jp.SIDEA.Models.Telefone;
-import com.jp.SIDEA.Models.Records.denunciajson;
 
 @Controller
 @RequestMapping("/denuncia")
 public class DenunciaController {
+
+
+    @Autowired
+    private DenunciaService denuncias;
+
     
     @GetMapping
     public ModelAndView denunciaView(){
@@ -22,6 +27,7 @@ public class DenunciaController {
 
     @PostMapping
     public ModelAndView denunciaView(@ModelAttribute denunciajson json){
+        denuncias.Salvar(json);
         ModelAndView modelinho = new ModelAndView("denuncia/denuncia");
         return modelinho;
     }
