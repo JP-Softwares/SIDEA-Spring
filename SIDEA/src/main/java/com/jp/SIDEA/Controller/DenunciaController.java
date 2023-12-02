8636d0b8a875c39fa220830dabea0727e0304169
Andre-Luiz01 <andreff.net@gmail.com>
2023-12-02 11:51:44 -0300

    teste

--- SIDEA/src/main/java/com/jp/SIDEA/Controller/DenunciaController.java
+++ SIDEA/src/main/java/com/jp/SIDEA/Controller/DenunciaController.java
@@ -1,15 +1,12 @@
 package com.jp.SIDEA.Controller;
 
-import com.jp.SIDEA.Models.Records.loginJson;
+import com.jp.SIDEA.Models.Records.denunciajson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.jp.SIDEA.Models.Telefone;
-import com.jp.SIDEA.Models.Records.denunciajson;
 
 @Controller
 @RequestMapping("/denuncia")
