9666853d7fe07d39ad6fbb28619d5e29009ea9e1
Rodrigo Dourado <rodrigodourado32@hotmail.com>
2023-12-02 19:17:07 -0300

    Improvment - Tela de Home

--- src/main/java/com/jp/SIDEA/Controller/TelaInicialController.java
+++ src/main/java/com/jp/SIDEA/Controller/TelaInicialController.java
@@ -2,17 +2,15 @@ package com.jp.SIDEA.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/Home")
 public class TelaInicialController {
     
 
     @GetMapping
     public ModelAndView homeView(){
-        ModelAndView modelinho = new ModelAndView("Home/index");
+        ModelAndView modelinho = new ModelAndView("home");
         return modelinho;
     }
 }
