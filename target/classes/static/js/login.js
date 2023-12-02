ad851a27f817538a3ac5ecfd8c39018fc5839f79
Geydson <geydson87@gmail.com>
2023-12-02 14:10:21 -0300

    teste

---
+++ target/classes/static/js/login.js
@@ -0,0 +1,27 @@
+var btnSignin = document.querySelector("#signin");
+var btnSignup = document.querySelector("#signup");
+var body = document.querySelector("body");
+
+/*
+btnSignin.addEventListener("click", function(){
+    body.className = "sign-in-js";
+})
+
+btnSignup.addEventListener("click", function (){
+    body.className = "sign-up-js";
+})
+document.addEventListener("DOMContentLoaded", function () {
+    // Aguarde o DOM ser completamente carregado
+
+    // Obtenha uma referência ao botão "Criar uma conta" pelo seu ID
+    var criarContaBtn = document.getElementById("createAccount");
+
+    // Adicione um ouvinte de evento de clique ao botão
+    criarContaBtn.addEventListener("click", function () {
+        // Adicione aqui as ações que deseja realizar quando o botão for clicado
+        window.location.href = "http://localhost:8080/Login/create";
+    })
+    
+})
+
+*/
