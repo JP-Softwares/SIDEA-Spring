b833f18e4d0d80d38edae52bc1fce9ca317bb8b8
Andre-Luiz01 <andreff.net@gmail.com>
2023-12-03 11:48:08 -0300

    Commit table

--- src/main/java/com/jp/SIDEA/Persistencia/DenunciaRepository.java
+++ src/main/java/com/jp/SIDEA/Persistencia/DenunciaRepository.java
@@ -2,12 +2,26 @@ package com.jp.SIDEA.Persistencia;
 
 
 import com.jp.SIDEA.Models.Denuncia;
+import com.jp.SIDEA.Models.Usuario;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
+@Transactional
 public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
 
     Denuncia findByProtocolo(String protocolo);
+
+
+    @Query("FROM Denuncia WHERE autor = :autor")
+    Optional<List<Denuncia>> listarTodosDoAutor(Usuario autor);
+
+    @Query("FROM Denuncia WHERE autor = :autor or executor = :autor")
+    Optional<List<Denuncia>> listarTodosDoUsuario(Usuario autor);
 
 }
