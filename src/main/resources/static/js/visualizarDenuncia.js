fef1c3b41aa481f572839b6aa467773f66132c75
Geydson <geydson87@gmail.com>
2023-12-03 14:43:36 -0300

    improves e correções

--- src/main/resources/static/js/visualizarDenuncia.js
+++ src/main/resources/static/js/visualizarDenuncia.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let fileName = "[[${anexo.nome_arquivo}]]";
-    let file = new File("[[${anexo.conteudo}]]", fileName,{type:"[[${anexo.tipo_arquivo}]]"});
-    let container = new DataTransfer(); 
+    let file = new File("[[${anexo.conteudo}]]", fileName, { type: "[[${anexo.tipo_arquivo}]]" });
+    let container = new DataTransfer();
     container.items.add(file);
     document.querySelector('#anexo').files = container.files;
 });
