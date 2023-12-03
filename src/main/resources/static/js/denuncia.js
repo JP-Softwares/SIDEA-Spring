5e17e6dd55c9abe0176c246701994373ab6d8658
Geydson <geydson87@gmail.com>
2023-12-03 12:52:04 -0300

    commit tela denuncia

---
+++ src/main/resources/static/js/denuncia.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#anexo").bind('change', handleFileSelect);
+});
+
+function handleFileSelect(evt) {
+    var filename;
+    try {
+        var files = evt.target.files;
+        filename = files[0].name;
+        $('#anexo-name').text(filename);
+    } catch (error) {
+        $('#anexo-name').text('Escolha um arquivo');
+    }
+}
